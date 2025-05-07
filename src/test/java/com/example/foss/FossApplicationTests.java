96dcfc6c3c7ff7b842b64e4f7b28dafbf5d28c18
azsx92 <azsx92@gmail.com>
2025-05-07 17:58:38 +0900

    firest commit

---
+++ src/test/java/com/example/foss/FossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FossApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
